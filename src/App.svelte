b07fdd05d41f2c7eca8cefdeb825f56891b43acb
Anas7866 <anaswaqar.dev@gmail.com>
2023-05-18 16:18:01 +0500

    new commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Router from "svelte-spa-router";
+  import Home from "./Routes/Home.svelte";
+  import About from "./Routes/About.svelte";
+
+  let routes = {
+    "/": Home,
+    "/about": About,
+  };
+</script>
+
+<main>
+  <Router {routes} />
+</main>
---
+++ src/Routes/Home.svelte
@@ -0,0 +1,3 @@
+<main>
+  <h1>Homw</h1>
+</main>
