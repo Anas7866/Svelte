add63b8588d95e32f2cb013fd6d3ab58b134b20c
Anas7866 <anaswaqar.dev@gmail.com>
2023-05-23 13:31:55 +0500

    new upload

--- src/Routes/About.svelte
+++ src/Routes/About.svelte
@@ -29,24 +29,185 @@
           />
         </div>
         <div class="col-md-12 px-md-5 px-2 py-5">
-          <span class="badge bg-success">Live</span>
-          <h1>Pittsburgh Collaborative x Goodr Grocery at Gideons Kindezi</h1>
-          <h2>05/01/2023 15:30</h2>
-          <h6>
-            The Pittsburgh Collaborative has partnered with Goodr to bring a
-            free supplemental food resource to Kindezi at Gideons.
-          </h6>
-          <div class="row pb-2">
-            <div class="col-md-1 col-1 text-center">
-              <span class="icon_marker"
-                ><i class="fas fa-map-marker-alt" /></span
-              >
+          <div class="row">
+            <div class="col-md-10">
+              <span class="badge bg-success">Live</span>
+              <h1>
+                Pittsburgh Collaborative x Goodr Grocery at Gideons Kindezi
+              </h1>
+              <h2>05/01/2023 15:30</h2>
+              <h6>
+                The Pittsburgh Collaborative has partnered with Goodr to bring a
+                free supplemental food resource to Kindezi at Gideons.
+              </h6>
+              <div class="row pb-2">
+                <div class="col-md-1 col-1 text-center">
+                  <span class="icon_marker"
+                    ><i class="fas fa-map-marker-alt" /></span
+                  >
+                </div>
+                <div class="col-md-11 col-11 pl-0">
+                  <h5>Gideons Elementary School</h5>
+                  <h6>897 Welch Street Southwest Atlanta, GA 30310</h6>
+                </div>
+              </div>
             </div>
-            <div class="col-md-11 col-11 pl-0">
-              <h5>Gideons Elementary School</h5>
-              <h6>897 Welch Street Southwest Atlanta, GA 30310</h6>
+            <div class="col-md-2 m-auto">
+              <!-- Button trigger modal -->
+              <button
+                type="button"
+                class="btn btn-success w-100"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                REGISTER NOW
+              </button>
+
+              <!-- Modal -->
+              <div
+                class="modal fade modal-xl"
+                id="exampleModal"
+                tabindex="-1"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
+                <div class="modal-dialog modal-dialog-centered">
+                  <div class="modal-content">
+                    <div class="modal-body p-0">
+                      <div class="col-md-12">
+                        <div class="row">
+                          <div class="col-md-8 padding_right_card">
+                            <div
+                              class="col-md-12 border_right py-4 text-center"
+                            >
+                              <h5>
+                                Pittsburgh Collaborative x Goodr Grocery at
+                                Gideons Kindezi
+                              </h5>
+                              <h6>Tuesday, May 23, 3:30 - 5pm EDT</h6>
+                            </div>
+                            <div>
+                              <div class="container py-5 border_right_lower">
+                                <div class="row">
+                                  <div class="col-md-2" />
+                                  <div class="col-md-8">
+                                    <div class="col-md-12 border_card">
+                                      <div class="row">
+                                        <div class="col-md-8">
+                                          <h6>
+                                            <b
+                                              >Parent Shopping Slot 3:30pm -
+                                              4:00pm</b
+                                            >
+                                          </h6>
+                                        </div>
+                                        <div class="col-md-4">
+                                          <div class="col-md-12 text-end">
+                                            <button class="btn btn-light btn-sm"
+                                              ><i class="fas fa-plus" /></button
+                                            >
+                                            <span>3</span>
+                                            <button class="btn btn-light btn-sm"
+                                              ><i
+                                                class="fas fa-minus"
+                                              /></button
+                                            >
+                                          </div>
+                                        </div>
+                                      </div>
+                                      <div class="row pt-4">
+                                        <div class="col-md-8">
+                                          <h6><b>Free</b></h6>
+                                        </div>
+                                        <div class="col-md-4 text-end">
+                                          <button
+                                            class="btn btn-light rounded-pill btn-sm px-3"
+                                          >
+                                            <b>Sold out</b>
+                                          </button>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div class="col-md-2" />
+                                </div>
+                                <div class="row pt-4">
+                                  <div class="col-md-2" />
+                                  <div class="col-md-8 position_adjust">
+                                    <div class="col-md-12 border_card">
+                                      <div class="row">
+                                        <div class="col-md-8">
+                                          <h6>
+                                            <b
+                                              >Parent Shopping Slot 3:30pm -
+                                              4:00pm</b
+                                            >
+                                          </h6>
+                                        </div>
+                                        <div class="col-md-4">
+                                          <div class="col-md-12 text-end">
+                                            <button class="btn btn-light btn-sm"
+                                              ><i class="fas fa-plus" /></button
+                                            >
+                                            <span>3</span>
+                                            <button class="btn btn-light btn-sm"
+                                              ><i
+                                                class="fas fa-minus"
+                                              /></button
+                                            >
+                                          </div>
+                                        </div>
+                                      </div>
+                                      <div class="row pt-4">
+                                        <div class="col-md-8">
+                                          <h6><b>Free</b></h6>
+                                        </div>
+                                        <div class="col-md-4 text-end">
+                                          <button
+                                            class="btn btn-light rounded-pill btn-sm px-3"
+                                          >
+                                            <b>Sold out</b>
+                                          </button>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div class="col-md-2" />
+                                </div>
+                              </div>
+                              <div class="modal-footer pt-5">
+                                <button
+                                  type="button"
+                                  data-bs-dismiss="modal"
+                                  class="btn btn-warning px-5 background_color_orange"
+                                >
+                                  <b>REGISTER</b>
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="col-md-4 padding_left_card text-center">
+                            <div class="col-md-12 pt-3">
+                              <img
+                                class="img-fluid"
+                                src="../Images/banner.jpg"
+                                alt=""
+                              />
+                            </div>
+                            <div class="col-md-12 background_cart_section">
+                              <i class="fas fa-shopping-cart" />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3318.173014052291!2d-84.40800462445632!3d33.73034198465926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5030e0cb9163d%3A0xc72663fb465c74ff!2s897%20Welch%20St%20SW%2C%20Atlanta%2C%20GA%2030310%2C%20USA!5e0!3m2!1sen!2s!4v1682680874007!5m2!1sen!2s"
             title=""
@@ -64,6 +225,25 @@
 </main>
 
 <style>
+  .background_color_orange {
+    background-color: rgb(209, 65, 12);
+    color: white;
+  }
+  .padding_right_card {
+    padding-right: 0px !important;
+  }
+  .padding_left_card {
+    padding-left: 0px !important;
+  }
+  .background_cart_section {
+    background-color: rgb(244, 244, 244);
+    height: 45vh;
+    padding-top: 50%;
+    color: lightgray;
+  }
+  .border_right_lower {
+    border-right: 1px solid lightgray !important;
+  }
   main {
     overflow-x: hidden;
     background-color: #f7f8f8;
@@ -86,35 +266,34 @@
     border-bottom: 1px solid lightgray;
   }
 
-  .navbar {
-    display: none;
+  .border_right {
+    border-right: 1px solid lightgray;
+    border-bottom: 1px solid lightgray !important;
   }
   .text_for_mobile {
     display: none;
+  }
+  .border_card {
+    border: 1px solid lightgray;
+    padding: 3%;
+    border-radius: 10px;
   }
 
   @media only screen and (max-width: 600px) {
     .background_of_navbar {
       display: none;
     }
+    .padding_right_card {
+      padding-right: auto !important;
+    }
+    .padding_left_card {
+      padding-left: auto !important;
+    }
     .text_for_mobile {
       display: block;
     }
-    .icons_car {
-      font-size: 160%;
-      color: #575f72;
-    }
     .img_logo_style {
       width: 35%;
-    }
-    .navbar {
-      overflow: hidden;
-      position: fixed;
-      bottom: 0;
-      width: 100%;
-      display: block;
-      border-top: 1px solid #575f7287;
-      background-color: white;
     }
   }
 </style>
